687a339498e45bcd7c3955dc7e6834d3b5b9caa9
Christoph Rist <rist.christoph@gmail.com>
2017-02-02 15:36:00 +0100

    Add UniformRandomSource

--- nimble/sources/elementary.py
+++ nimble/sources/elementary.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from ..composition import SeekableSource
+from ..core import Source, SeekableSource
 
 import numpy as np
 
@@ -21,9 +21,23 @@ class IntegerSource(SeekableSource):
         super(IntegerSource, self).__init__(name=name)
 
     def _get_data_at(self, position):
-
         return np.array([self._start + position * self._step], dtype=self._dtype)
 
-    @property
-    def dtype(self):
-        return self._dtype
+
+class UniformRandomSource(Source):
+    """Random values in a given shape."""
+
+    def __init__(self, shape, name=u"", **kwargs):
+        self.parallel_possible = True
+        self.cached = False
+        self._shape = shape
+        self._dtype = np.float64
+        self._cache = None
+        super(UniformRandomSource, self).__init__(name=name, **kwargs)
+
+    def _get_data_impl(self):
+        return self._cache
+
+    def advance(self):
+        self._cache = np.random.rand(self._shape)
+        return True
